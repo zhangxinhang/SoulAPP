121a7de81fbc99768051d5355190d3620b355141
ams-zxh <zxh@e-teng.com.cn>
2017-04-25 17:40:22 +0800

    add http example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import { OnInit } from '@angular/core';
+import { HomeService } from './home.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise'
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [HomeService]
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  data = {};
+  constructor(public navCtrl: NavController,private homeService:HomeService,private http:Http) {
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  ngOnInit(): void {
+    console.log('homepage init');    
+  }
+ 
+  clickNews():void {
+    console.log('click news');
+    this.http.get('/mvapi/mobile/sharelink').toPromise().then(function(result){
+      console.log(result);
+    })
   }
 
 }
